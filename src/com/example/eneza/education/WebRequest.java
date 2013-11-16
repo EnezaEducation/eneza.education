80f061911ac9b90f29238eff17b2bd34899bff04
radu <rf59@st-andrews.ac.uk>
2013-11-16 19:40:17 +0000

    Changed the WebRequest class. Hope it works like that.

--- src/com/example/eneza/education/WebRequest.java
+++ src/com/example/eneza/education/WebRequest.java
@@ -1,35 +1,57 @@
 package com.example.eneza.education;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 
-import android.util.Log;
+import android.os.AsyncTask;
 
 public class WebRequest {
-	
-	public static String callWebService(String URL, String deviceId, String query) {
-		String result = "";
-        HttpClient httpclient = new DefaultHttpClient();  
-        HttpGet request = new HttpGet(URL + query);  
-        request.addHeader("deviceId", deviceId);  
-        ResponseHandler<String> handler = new BasicResponseHandler();  
-        try {  
-            result = httpclient.execute(request, handler);  
-        } catch (ClientProtocolException e) {  
-            e.printStackTrace();  
-        } catch (IOException e) {  
-            e.printStackTrace();  
-        }  
-        httpclient.getConnectionManager().shutdown();  
-        Log.i("WebRequest - callWebService", result);
-        return result;
+	public static class Request extends AsyncTask<String, String, String>
+    {
+        @Override
+        protected String doInBackground(String... arg)
+        {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+            try
+            {
+                response = httpclient.execute(new HttpGet(arg[0]));
+                StatusLine statusLine = response.getStatusLine();
+                if(statusLine.getStatusCode() == HttpStatus.SC_OK)
+                {
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+                    responseString = out.toString();
+                    out.close();
+                }
+                else
+                {
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+            }
+            catch (ClientProtocolException e)
+            {
+            }
+            catch (IOException e)
+            {
+            }
+            return responseString;
+        }
+
+        @Override
+        protected void onPostExecute(String result)
+        {
+            super.onPostExecute(result);
+        }
     }
-	
-	
 }
