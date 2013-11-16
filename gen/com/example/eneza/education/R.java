b16981f1f53d5be94102afe3ad9ea6c8710fc025
Stefan Gladkov <stefan.gladkov@gmail.com>
2013-11-16 22:27:25 +0000

    sadas

--- gen/com/example/eneza/education/R.java
+++ gen/com/example/eneza/education/R.java
@@ -22,26 +22,29 @@ public final class R {
     }
     public static final class drawable {
         public static final int grayscale_logo=0x7f020000;
+        public static final int official_logo_square=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080011;
-        public static final int button1=0x7f080008;
-        public static final int button2=0x7f08000a;
-        public static final int button3=0x7f080009;
-        public static final int button4=0x7f08000b;
-        public static final int console=0x7f080005;
-        public static final int email=0x7f08000f;
-        public static final int emailIP=0x7f080010;
-        public static final int loginBtn=0x7f080004;
-        public static final int name=0x7f08000d;
-        public static final int nameIP=0x7f08000c;
-        public static final int passwordIP=0x7f080003;
-        public static final int phoneIP=0x7f08000e;
-        public static final int sendBtn=0x7f080006;
-        public static final int signup=0x7f080000;
-        public static final int userName=0x7f080001;
-        public static final int userNameIP=0x7f080002;
-        public static final int userTextIp=0x7f080007;
+        public static final int action_settings=0x7f080013;
+        public static final int appName=0x7f080000;
+        public static final int button1=0x7f08000a;
+        public static final int button2=0x7f08000c;
+        public static final int button3=0x7f08000b;
+        public static final int button4=0x7f08000d;
+        public static final int console=0x7f080007;
+        public static final int email=0x7f080011;
+        public static final int emailIP=0x7f080012;
+        public static final int imageView1=0x7f080001;
+        public static final int loginBtn=0x7f080006;
+        public static final int name=0x7f08000f;
+        public static final int nameIP=0x7f08000e;
+        public static final int passwordIP=0x7f080005;
+        public static final int phoneIP=0x7f080010;
+        public static final int sendBtn=0x7f080008;
+        public static final int signup=0x7f080002;
+        public static final int userName=0x7f080003;
+        public static final int userNameIP=0x7f080004;
+        public static final int userTextIp=0x7f080009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
