2d501dbf840a5626eefb883be048e6121daab270
radu <rf59@st-andrews.ac.uk>
2013-11-16 18:46:55 +0000

    gsonRegistrationStruct package added. Registration class contains code for creating a JSON object which will be sent to the server for the registration task. Also, that class's constructor can be used to create JSON object from UI Form. RegistrationResponse will parse a JSON object to get the actual data (userID and username).

--- src/com/example/eneza/education/GSon.java
+++
@@ -1,5 +0,0 @@
-package com.example.eneza.education;
-
-public class GSon {
-//	public static 
-}
---
+++ src/gsonRegistrationStruct/IncorrectParametersException.java
@@ -0,0 +1,5 @@
+package gsonRegistrationStruct;
+
+public class IncorrectParametersException extends Exception {
+	private static final long serialVersionUID = 1L;
+}
