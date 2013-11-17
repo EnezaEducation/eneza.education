4fc66da52e2497844233c529139f3683305c7856
radu <rf59@st-andrews.ac.uk>
2013-11-17 01:13:23 +0000

    working one

--- src/com/example/eneza/education/MainActivity.java
+++ src/com/example/eneza/education/MainActivity.java
@@ -1,16 +1,31 @@
 package com.example.eneza.education;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	Button signUp;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         setContentView(R.layout.activity_main);
+        signUp = (Button) findViewById(R.id.signup);
+        signUp.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				Intent act2 = new Intent(getApplicationContext(), SignUp.class);
+                startActivity(act2);
+			}
+        	
+        });
     }
 
 
